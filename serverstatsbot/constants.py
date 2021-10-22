36bac2698f9a54b8679834daf5d9ae206642904c
Zach <26696119+bergeo-ogg@users.noreply.github.com>
2021-10-22 20:42:07 +0100

    Update constants.py

--- serverstatsbot/constants.py
+++ serverstatsbot/constants.py
@@ -1,3 +1,4 @@
-prefix = "++"
+API_ENDPOINT = "https://nktzz4aizu-dsn.algolia.net/1/indexes/*/queries"
+API_HEADERS = {"Content-Type": "application/json", "x-algolia-api-key": "aca0d7082e4e63af5ba5917d5e96bed0", "x-algolia-application-id": "NKTZZ4AIZU"}
 
-DISCORD_EPOCH = 1420070400000
+prefix = "++"
